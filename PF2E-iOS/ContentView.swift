02265c8384e0d16d4a643ebd99b9b2536a25962d
devvz <devvz@hotmail.com>
2020-11-19 09:28:09 -0500

    Initial Commit

---
+++ PF2E-iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PF2E-iOS
+//
+//  Created by Patrick Michael Kerrigan on 11/19/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PF2E-iOS/PF2E_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  PF2E_iOSApp.swift
+//  PF2E-iOS
+//
+//  Created by Patrick Michael Kerrigan on 11/19/20.
+//
+
+import SwiftUI
+
+@main
+struct PF2E_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
