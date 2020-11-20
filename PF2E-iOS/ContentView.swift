a3c6913c25c7a4f98bdedd13dbd22bc47b154c05
devvz <devvz@hotmail.com>
2020-11-20 15:52:52 -0500

    Recreated UI

--- PF2E-iOS/ContentView.swift
+++ PF2E-iOS/ContentView.swift
@@ -7,10 +7,42 @@
 
 import SwiftUI
 
+struct StartScreen: View {
+    var body: some View {
+        ZStack {
+            Image("background")
+                .resizable()
+                .scaledToFill()
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            
+            VStack {
+                Image("logo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding(.bottom, -20.0)
+                    .offset(y: 50)
+                Image("logo_text")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .offset(y: 50)
+                VStack {
+                    Spacer()
+                    Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+                        Image("start_button")
+                            .resizable()
+                            .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fit/*@END_MENU_TOKEN@*/)
+                            .padding(.horizontal, 50.0)
+                            .offset(y: -50)
+                    }
+                }
+            }
+        }
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        StartScreen()
     }
 }
 
---
+++ PF2E-iOS/NewLoadScreen.swift
@@ -0,0 +1,20 @@
+//
+//  NewLoadScreen.swift
+//  PF2E-iOS
+//
+//  Created by Pat on 11/20/20.
+//
+
+import SwiftUI
+
+struct NewLoadScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NewLoadScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        NewLoadScreen()
+    }
+}
